837063b771dc5ade1ce01702b14fc38aa17f59c0
durai <durai145@live.in>
2015-06-24 03:54:29 +0000

    first commit

---
+++ proxy.c
@@ -0,0 +1,202 @@
+/* 
+ * tcpserver.c - A simple TCP echo server 
+ * usage: tcpserver <port>
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#define BUFSIZE 1024
+
+#if 0
+/* 
+ * Structs exported from in.h
+ */
+
+/* Internet address */
+struct in_addr {
+  unsigned int s_addr; 
+};
+
+/* Internet style socket address */
+struct sockaddr_in  {
+  unsigned short int sin_family; /* Address family */
+  unsigned short int sin_port;   /* Port number */
+  struct in_addr sin_addr;	 /* IP address */
+  unsigned char sin_zero[...];   /* Pad to size of 'struct sockaddr' */
+};
+
+/*
+ * Struct exported from netdb.h
+ */
+
+/* Domain name service (DNS) host entry */
+struct hostent {
+  char    *h_name;        /* official name of host */
+  char    **h_aliases;    /* alias list */
+  int     h_addrtype;     /* host address type */
+  int     h_length;       /* length of address */
+  char    **h_addr_list;  /* list of addresses */
+}
+#endif
+
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  int parentfd; /* parent socket */
+  int childfd; /* child socket */
+  int portno; /* port to listen on */
+  int clientlen; /* byte size of client's address */
+  struct sockaddr_in serveraddr; /* server's addr */
+  struct sockaddr_in clientaddr; /* client addr */
+  struct hostent *hostp; /* client host info */
+  char buf[BUFSIZE]; /* message buffer */
+  char *hostaddrp; /* dotted decimal host addr string */
+  int optval; /* flag value for setsockopt */
+  int n; /* message byte size */
+
+
+
+int clientSocket;
+  char buffer[1024];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
+
+
+
+  /* 
+   * check command line arguments 
+   */
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+  portno = atoi(argv[1]);
+
+  /* 
+   * socket: create the parent socket 
+   */
+  parentfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (parentfd < 0) 
+    error("ERROR opening socket");
+
+  /* setsockopt: Handy debugging trick that lets 
+   * us rerun the server immediately after we kill it; 
+   * otherwise we have to wait about 20 secs. 
+   * Eliminates "ERROR on binding: Address already in use" error. 
+   */
+  optval = 1;
+  setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, 
+	     (const void *)&optval , sizeof(int));
+
+clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+/* Address family = Internet */
+  serverAddr.sin_family = AF_INET;
+  /* Set port number, using htons function to use proper byte order */
+  serverAddr.sin_port = htons(8090);
+  /* Set IP address to localhost */
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  /* Set all bits of the padding field to 0 */
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+
+
+
+
+addr_size = sizeof serverAddr;
+  if( connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size) <0)
+  {
+    fprintf(stderr,"Error when connecting! %s\n",strerror(errno)); 
+    exit(1);
+  }
+
+  /*
+   * build the server's Internet address
+   */
+  bzero((char *) &serveraddr, sizeof(serveraddr));
+
+  /* this is an Internet address */
+  serveraddr.sin_family = AF_INET;
+
+  /* let the system figure out our IP address */
+  serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  /* this is the port we will listen on */
+  serveraddr.sin_port = htons((unsigned short)portno);
+
+  /* 
+   * bind: associate the parent socket with a port 
+   */
+  if (bind(parentfd, (struct sockaddr *) &serveraddr, 
+	   sizeof(serveraddr)) < 0) 
+    error("ERROR on binding");
+
+  /* 
+   * listen: make this socket ready to accept connection requests 
+   */
+  if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
+    error("ERROR on listen");
+
+  /* 
+   * main loop: wait for a connection request, echo input line, 
+   * then close connection.
+   */
+  clientlen = sizeof(clientaddr);
+  while (1) {
+
+   
+    childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+    if (childfd < 0) 
+      error("ERROR on accept");
+    
+    hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, 
+			  sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+    if (hostp == NULL)
+      error("ERROR on gethostbyaddr");
+
+
+    hostaddrp = inet_ntoa(clientaddr.sin_addr);
+    if (hostaddrp == NULL)
+      error("ERROR on inet_ntoa\n");
+    printf("server established connection with %s (%s)\n", 
+	   hostp->h_name, hostaddrp);
+    
+   
+    bzero(buf, BUFSIZE);
+    n = read(childfd, buf, BUFSIZE);
+    if (n < 0) 
+      error("ERROR reading from socket");
+    fprintf(stderr,"server received %d bytes: %s", n, buf);
+
+
+    n=send(clientSocket, buf, n, 0);
+    fprintf(stderr,"\n ### send to ecod %d bytes: %s", n, buf);
+
+	
+    n=recv(clientSocket, buffer, n, 0);
+    fprintf(stderr,"\n ### received  to ecod %d bytes: %s", n,buffer);
+    
+    /* 
+     * write: echo the input string back to the client 
+     */
+    n = write(childfd, buffer, strlen(buf));
+    if (n < 0) 
+      error("ERROR writing to socket");
+close(childfd);
+    
+  }
+
+  
+}
